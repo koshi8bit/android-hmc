8db2d5109e1812c9fc8fe9e73cedd37527b12b54
koshi8bit 101 <koshi8bit@mail.ru>
2022-02-03 19:11:49 +0700

    init

---
+++ app/src/main/java/com/koshi8bit/hmc/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.koshi8bit.hmc
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private val barcodeLauncher = registerForActivityResult(
+        ScanContract()
+    ) { result: ScanIntentResult ->
+        if (result.contents == null) {
+            Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(
+                this@MainActivity,
+                "Scanned: " + result.contents,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    // Launch
+    fun onButtonClick(view: View?) {
+        // barcodeLauncher.launch(ScanOptions())
+        val intent = Intent(this, ContinuousCaptureActivity::class.java)
+        startActivity(intent)
+    }
+}
