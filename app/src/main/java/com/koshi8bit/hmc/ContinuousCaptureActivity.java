d915c33ce97c55e22f2a641a0dcfff91e13be401
koshi8bit 101 <koshi8bit@mail.ru>
2022-02-03 20:30:25 +0700

    v0.9

--- app/src/main/java/com/koshi8bit/hmc/ContinuousCaptureActivity.java
+++ app/src/main/java/com/koshi8bit/hmc/ContinuousCaptureActivity.java
@@ -1,8 +1,10 @@
 package com.koshi8bit.hmc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -15,9 +17,12 @@ import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 import com.journeyapps.barcodescanner.DefaultDecoderFactory;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * This sample performs continuous scanning, displaying the barcode and source image whenever
@@ -27,32 +32,37 @@ public class ContinuousCaptureActivity extends Activity {
     private DecoratedBarcodeView barcodeView;
     private BeepManager beepManager;
     private String lastText;
+    private ImageView imageView;
+    private ArrayList<String> list;
 
     private BarcodeCallback callback = new BarcodeCallback() {
         @Override
         public void barcodeResult(BarcodeResult result) {
-            if(result.getText() == null || result.getText().equals(lastText)) {
+            String res = result.getText();
+            if(res == null || res.equals(lastText)) {
                 // Prevent duplicate scans
                 return;
             }
 
-            lastText = result.getText();
-            barcodeView.setStatusText(result.getText());
+            lastText = res;
+            barcodeView.setStatusText(res);
 
             beepManager.playBeepSoundAndVibrate();
+            list.add(res);
 
             //Added preview of scanned barcode
-            ImageView imageView = findViewById(R.id.barcodePreview);
             imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
         }
 
         @Override
         public void possibleResultPoints(List<ResultPoint> resultPoints) {
+            // Log.d("3", "4");
         }
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("AAAAAAAAA", "onCreate");
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.continuous_scan);
@@ -62,6 +72,9 @@ public class ContinuousCaptureActivity extends Activity {
         barcodeView.getBarcodeView().setDecoderFactory(new DefaultDecoderFactory(formats));
         barcodeView.initializeFromIntent(getIntent());
         barcodeView.decodeContinuous(callback);
+
+        imageView = findViewById(R.id.barcodePreview);
+        list = new ArrayList<>();
 
         beepManager = new BeepManager(this);
     }
@@ -94,6 +107,28 @@ public class ContinuousCaptureActivity extends Activity {
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        return barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+        boolean res = barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+        if (res) {
+            Set<String> set = new HashSet<>(list);
+            list.clear();
+            list.addAll(set);
+
+            Intent intent = new Intent();
+            intent.putExtra(Intent.EXTRA_TEXT, String.join("\n", list));
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+        return res;
     }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        // check that it is the SecondActivity with an OK result
+//        if (requestCode == RESULT_OK) {
+//            String returnString = data.getStringExtra(Intent.EXTRA_TEXT);
+//        }
+//
+//    }
 }
--- app/src/main/java/com/koshi8bit/hmc/MainActivity.kt
+++ app/src/main/java/com/koshi8bit/hmc/MainActivity.kt
@@ -1,38 +1,60 @@
 package com.koshi8bit.hmc
 
+import android.R.attr.label
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.journeyapps.barcodescanner.ScanContract
-import com.journeyapps.barcodescanner.ScanIntentResult
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var textView_qrs: TextView? = null
+    val QR_SCAN_INDENT = 10
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView_qrs = findViewById(R.id.textView_qrs)
     }
 
-    private val barcodeLauncher = registerForActivityResult(
-        ScanContract()
-    ) { result: ScanIntentResult ->
-        if (result.contents == null) {
-            Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_LONG).show()
-        } else {
-            Toast.makeText(
-                this@MainActivity,
-                "Scanned: " + result.contents,
-                Toast.LENGTH_LONG
-            ).show()
-        }
-    }
-
-    // Launch
     fun onButtonClick(view: View?) {
         // barcodeLauncher.launch(ScanOptions())
         val intent = Intent(this, ContinuousCaptureActivity::class.java)
-        startActivity(intent)
+        startActivityForResult(intent, QR_SCAN_INDENT)
     }
+
+    fun copyToClipboard(text: CharSequence){
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("label",text)
+        clipboard.setPrimaryClip(clip)
+        Toast.makeText(this, getString(R.string.copyed), Toast.LENGTH_LONG).show()
+    }
+
+    fun onCopyClick(view: View?) {
+        copyToClipboard(textView_qrs?.text ?: "")
+    }
+
+    @Override
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // check that it is the SecondActivity with an OK result
+        if (requestCode == QR_SCAN_INDENT) {
+            if (resultCode == RESULT_OK) {
+                val returnString = data!!.getStringExtra(Intent.EXTRA_TEXT)
+                textView_qrs?.text = returnString
+                copyToClipboard(textView_qrs?.text ?: "")
+
+            }
+        }
+    }
+
 }
